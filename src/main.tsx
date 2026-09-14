ba72a7f049bc86c7ad787367e72342867189ae00
webbcpapin <webbcpapin@gmail.com>
2026-09-15 05:20:48 +0700

    fix: surface Apps Script HTML deployment errors

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './v3/responsePatch'
 import AppV3 from './v3/AppV3.tsx'
 
 createRoot(document.getElementById('root')!).render(
